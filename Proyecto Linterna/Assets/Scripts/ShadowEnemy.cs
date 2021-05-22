e27f9044db8eaafdfb607bddf6e409b79ee90bd8
Belu123mm <32393295+Belu123mm@users.noreply.github.com>
2021-05-22 17:19:11 -0300

    Inicio de enemigo

---
+++ Proyecto Linterna/Assets/Scripts/ShadowEnemy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowEnemy : MonoBehaviour
+{
+    FSM<ShadowStates> _fsm;
+    public Transform followed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetStates();
+    }
+    void SetStates()
+    {
+        var idle = new IdleState<ShadowStates>();
+        var walk = new WalkState<ShadowStates>();
+        var attack = new AttackState<ShadowStates>();
+
+        walk.Execute = Walking;
+
+
+        idle.AddTransition(ShadowStates.walk, walk);
+        idle.AddTransition(ShadowStates.attack, attack);
+
+        walk.AddTransition(ShadowStates.idle, idle);
+        walk.AddTransition(ShadowStates.attack, attack);
+
+        attack.AddTransition(ShadowStates.idle, idle);
+        attack.AddTransition(ShadowStates.walk, walk);
+
+        _fsm = new FSM<ShadowStates>();
+        _fsm.SetInit(walk);
+
+    }
+    // Update is called once per frame
+    void Walking()
+    {
+        var dir = followed.position - transform.position;
+        transform.position += dir.normalized;
+    }
+    void Update()
+    {
+        _fsm.OnUpdate();
+    }
+}
+public enum ShadowStates
+{
+    idle,
+    walk,
+    attack
+}
