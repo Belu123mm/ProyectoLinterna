ab68dc36f594ef2cee594aaf39aa165e8493cb44
Fabricio salas <FabrIICB@gmail.com>
2022-02-23 17:54:31 -0300

    Puzzle, iluminación, animaciones

---
+++ Proyecto Linterna/Assets/PopText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopText : MonoBehaviour
+{
+    
+     public Transform PlayerObject;
+    public float DistanceToView = 10f;
+    private TextMesh myTextMesh;
+    private string MyOriginalText;
+
+    void Start()
+    {
+        myTextMesh = GetComponent<TextMesh>();
+        MyOriginalText = myTextMesh.text;
+    }
+    void Update()
+    {
+        if (Vector3.Distance(PlayerObject.position, transform.position) <= DistanceToView)
+        {
+            myTextMesh.text = MyOriginalText;
+        }
+        else
+        {
+            myTextMesh.text = "";
+        }
+    }
+}
+
