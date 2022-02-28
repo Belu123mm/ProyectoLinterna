0284291732ce24800a67134a5b00f316b86101a5
Belen Nava <32393295+Belu123mm@users.noreply.github.com>
2022-02-28 05:37:50 -0300

    Screamer

---
+++ Proyecto Linterna/Assets/Scripts/Screamer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screamer : MonoBehaviour
+{
+    public float screamTime;
+    public AudioSource scream;
+    public GameObject player;
+    public GameObject screamCamera;
+    private void OnTriggerEnter(Collider other)
+    {
+        scream.Play();
+        player.SetActive(false);
+        screamCamera.SetActive(true);
+        StartCoroutine(EndScream());
+    }
+    IEnumerator EndScream()
+    {
+        yield return new WaitForSeconds(screamTime);
+        player.SetActive(true);
+        screamCamera.SetActive(false);
+    }
+}
